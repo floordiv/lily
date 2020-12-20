cd834bfa254f540e7cbd112c876e860470e51920
floordiv <pavloromanenko25@gmail.com>
2020-12-20 04:01:22 +0200

    added ljson parser blank (dictionary implementation required to complete parser)

--- lib/std/ljson.py
+++ lib/std/ljson.py
@@ -1,1 +1,7 @@
 from lily.core.lexer.lexer import Lexer
+
+
+def parse(raw_json, context=None):
+    lexer = Lexer(raw_json, context=context)
+    lexemes = lexer.parse()
+    # TODO: complete json parser after dictionaries implementation
